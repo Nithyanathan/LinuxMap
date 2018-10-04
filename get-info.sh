d5271ece327492ffa0cb8f0b100c546607a0d6b2
Ajay Kundnani <ajkundna@microsoft.com>
2018-10-04 20:16:30 +0530

    Updated help file

--- get-info.sh
+++ get-info.sh
@@ -1,3 +1,27 @@
+######################################################################################
+## Author: Cloud Modernization - Delivery <CloudMo-Delivery@microsoft.com>
+## Version: 0.2 	Date: 10/4/2018
+## The script is used to collect following information from Rehat/Centos Linux System
+## 1. BIOS and Serial Information
+## 2. Disk Info and Disk Layout  
+## 3. Block Device Details
+## 4. File System and Disk Usage
+## 5. Current Patching Level of the system
+## 6. Serial Information
+######################################################################################
+###############  How to use this script file  ########################################
+##  1. Copy the file and rename get-info.sh
+##  2. If permission denies set the execute permission using the below commands 
+##      chmod u+x get-info.sh 
+##  3. Check using the command: ls -l get-info.sh to check the permission applied
+##  4. Create a file as Output.txt file at /tmp direcotry which contains output 
+##  5. Execute the command on going to location 
+##      [root@mcdphlputl1502 ~]# ./get-info.sh 
+##  6. Check the package for executing Mailx command: rpm -q redhat-lsb
+######################################################################################
+
+############### Starting of the script ###############################################
+
 #!/bin/bash
 
 hostname=`sudo uname -n`

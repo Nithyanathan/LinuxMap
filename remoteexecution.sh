c908583d962b8b728211138cc9be1d4298e77bc2
Ajay Kundnani <ajkundna@microsoft.com>
2018-10-07 21:09:17 +0530

    Updated remoteexecution

--- remoteexecution.sh
+++ remoteexecution.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-username=xxxx
-password=xxxx
+username="akundnani"
+password="Microsoft~123"
 
-for host in `cat /tmp/servers.txt`; do
-    ssh $username@$host $password "bash -s" < ./backup.sh &
-done
-wait
+check_script() {
+  cat /tmp/get-info.sh
+  isscript=${?}
+  cat /tmp/servers.txt
+  isserverlist=${?}
+}
+
+check_script;
+if [$isscript -ne 0]; then
+    echo "Script get-info.sh not found under /tmp location, please copy script and try again"
+elif [$isserverlist -ne 0]; then
+    echo "Server list servers.txt not found under /tmp location, please create servers.txt and try again"
+else
+    #Create folder for files
+    mkdir -p /tmp/cloudmo/
+
+    for host in `cat /tmp/servers.txt`; do
+        ssh $username@$host $password "bash -s" < /tmp/get-info.sh &
+    done
+    wait
+fi

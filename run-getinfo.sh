34151b1c822556561412e6a4e43881c88d90568f
Ajay Kundnani <ajkundna@microsoft.com>
2018-10-07 21:16:51 +0530

    Added new script

--- run-getinfo.sh
+++ run-getinfo.sh
@@ -4,19 +4,20 @@ username="akundnani"
 password="Microsoft~123"
 
 check_script() {
-  cat /tmp/get-info.sh
-  isscript=${?}
-  cat /tmp/servers.txt
-  isserverlist=${?}
+    cat /tmp/get-info.sh
+    isscript=${?}
+    cat /tmp/servers.txt
+    isserverlist=${?}
 }
 
 check_script;
 if [$isscript -ne 0]; then
-    echo "Script get-info.sh not found under /tmp location, please copy script and try again"
+    echo "Script not found under /tmp. Check again"
+    exit 1
 elif [$isserverlist -ne 0]; then
-    echo "Server list servers.txt not found under /tmp location, please create servers.txt and try again"
+    echo "Server list not found under /tmp. Check again"
+    exit 1
 else
-    #Create folder for files
     mkdir -p /tmp/cloudmo/
 
     for host in `cat /tmp/servers.txt`; do

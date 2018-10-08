8c68a050f5e74bc1e7bc7a72ee11fe149bef3c6d
Ajay Kundnani <ajkundna@microsoft.com>
2018-10-08 14:57:43 +0530

    Updated  script for running on multiple computers

--- run-getinfo-withoutsshpass.sh
+++ run-getinfo-withoutsshpass.sh
@@ -1,15 +1,12 @@
 ######################################################################################
 ## Author: Cloud Modernization - Delivery <CloudMo-Delivery@microsoft.com>
 ## Version: 0.2 	Date: 10/4/2018
-##  Pre-Requisites
-##  1. sshpass
 ######################################################################################
 
 ############### Starting of the script ###############################################
 #!/bin/bash
 
 username="akundnani"
-password="Microsoft~123"
 
 check_script() {
     cat /tmp/get-info.sh
@@ -30,14 +27,12 @@ else
 
     for host in `cat /tmp/servers.txt`; do
         echo "Running get-info.sh script on $host"
-        sshpass -p $password ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 $username@$host "bash -s" < /tmp/get-info.sh &
+        ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 $username@$host "bash -s" < /tmp/get-info.sh
     done
-    wait
     for host in `cat /tmp/servers.txt`; do
         echo "Copying files from $host to /tmp/cloudmo"
-        sshpass -p $password scp $username@$host:/tmp/cloudmo* /tmp/cloudmo/
+        scp $username@$host:/tmp/cloudmo* /tmp/cloudmo/
     done
-    wait
 fi
 
 echo "Scan complete, please copy /tmp/cloudmo files to your local machine for analysis."
